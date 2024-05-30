14ba9196cf19b4d55bf31d356773c70d410ed767
nafees nazik <nafeesnazik21@gmail.com>
2024-05-30 19:42:20 +0530

    feat: add layout component

---
+++ app/routes/_dashboard+/_layout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "@remix-run/react";
+import { Sidebar } from "./__components/sidebar";
+
+export default function DashboardLayout() {
+  return (
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+        <Sidebar />
+      </aside>
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
